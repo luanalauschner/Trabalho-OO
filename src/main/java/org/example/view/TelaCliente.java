934d7e85b971a1a12c374fe73b6bb3c79e780549
lanassilva0 <lanassilvalss@gmail.com>
2023-12-06 18:00:45 -0300

    inicialização tela_cliente

--- src/main/java/org/example/view/TelaCliente.java
+++ src/main/java/org/example/view/TelaCliente.java
@@ -5,10 +5,51 @@
  */
 package org.example.view;
 
+import javax.swing.JFrame;
+
+//importação do package de controller
+import org.example.controller.*;
+import org.example.controller.controllerAdiciona.AdicionaCliente;
+import org.example.controller.controllerAdiciona.AdicionaCarro;
+import org.example.controller.controllerAdiciona.AdicionaFilial;
+import org.example.controller.controllerAdiciona.AdicionaFuncionario;
+import org.example.controller.controllerSeleciona.SelecionaCarros;
+import org.example.controller.controllerSeleciona.SelecionaClientes;
+import org.example.controller.controllerSeleciona.SelecionaFiliais;
+import org.example.controller.controllerSeleciona.SelecionaFuncionarios;
+import org.example.controller.controllerSeleciona.SelecionaLocacoes;
+import org.example.controller.controllerSeleciona.SelecionaReservas;
+
+//importação do package exception
+import org.example.exception.FormatoException;
+
+//importação do package model
+import org.example.model.*;
+
+//importação da biblioteca utilizada
+import javax.swing.*;
+import javax.swing.event.*;
+
+//importação dos packages do awt utilizados
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Lana S. Silva
  */
 public class TelaCliente {
+
+    private JFrame tela_cliente;
+    private final int WIDTH = 500;
+    private final int HEIGHT = 300;
+    private final int V_GAP = 10;
+    private final int H_GAP = 5;
+
+
     
 }
